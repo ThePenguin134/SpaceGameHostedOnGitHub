c137d29558bd9a8f7a55e897edd2c0e1a1783ae9
ThePenguin134 <thepenguin134@gmail.com>
2023-09-25 12:06:45 -0500

    commit 3

---
+++ CenterRoom.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //NOTE: MANY OF THESE FOR LOOPS ARE UNNECESSARY AS THEY ONLY RUN ONCE, BUT THEY MAKE IT EASIER TO ADJUST AND COUNT SPACING
+//Top Row --------------------------------------------------------------------------------------------------------------
+    //prints "/" with proper spacing
+    cout << "  /   ";
+    //First for loop (1-5)
+    for (int a = 1; a <= 5; a++) {
+        //Prints asterisk 5 times
+        cout << "*";
+        //Inner for loop (3 times)
+        for (int b = 1; b <= 3; b++) {
+            //Prints 3 spaces between each asterisk
+            cout << " ";
+        }
+    }
+    //prints the door
+    cout << "| |   ";
+    //Second for loop (1-5)
+    for (int d = 1; d <= 5; d++) {
+        //Prints asterisk 5 times
+        cout << "*";
+        //Inner for loop (3 times)
+        for (int y = 1; y <= 3; y++) {
+            //Prints 3 spaces between each asterisk
+            cout << " ";
+        }
+    }
+    //prints the corner of the room
+    cout << "\\";
+    //new line
+    cout << endl;
+//Row 2 ----------------------------------------------------------------------------------------------------------------
+    //prints the corner of the room
+    cout << " /";
+//Third for loop (1-51)
+    for (int n = 1; n <= 51; n++) {
+        //Prints 51 spaces
+        cout << " ";
+    }
+    //prints the corner of the room
+    cout << "\\";
+    //new line
+    cout << endl;
+//Row 3 ----------------------------------------------------------------------------------------------------------------
+    //prints the corner of the room
+    cout << "/";
+    //Third for loop (1-53)
+    for (int n = 1; n <= 53; n++) {
+        //prints 53 spaces
+        cout << " ";
+    }
+    //prints the corner of the room
+    cout << "\\";
+    //new line
+    cout << endl;
+//Rows 4-8 -------------------------------------------------------------------------------------------------------------
+    //Fourth for loop (1-5)
+    for (int k = 1; k <= 5; k++) {
+        //Inner for loop (1 time)
+        for (int m = 1; m <= 1; m++) {
+            //prints *
+            cout << "*";
+        }
+        //Inner for loop (53 times)
+        for (int n = 1; n <= 53; n++) {
+            //prints 53 spaces
+            cout << " ";
+        }
+        //Inner for loop (1 time)
+        for (int o = 1; o <= 1; o++) {
+            //prints *
+            cout << "*";
+        }
+        //new line
+        cout << endl;
+    }
+//Rows 9 & 10 ----------------------------------------------------------------------------------------------------------
+    //Fifth for loop (1-2)
+    for (int a = 1; a <= 2; a++) {
+        //Inner for loop (1 time)
+        for (int m = 1; m <= 1; m++) {
+            //prints _
+            cout << "_";
+        }
+        //Inner for loop (53 times)
+        for (int n = 1; n <= 53; n++) {
+            //Prints 53 spaces
+            cout << " ";
+        }
+        //Inner for loop (1 time)
+        for (int o = 1; o <= 1; o++) {
+            //Prints _
+            cout << "_" << endl;
+        }
+        //new line
+        cout << endl;
+    }
+//Rows 11-15 -----------------------------------------------------------------------------------------------------------//Fourth for loop (1-5)
+    //Sixth for loop (1-5)
+    for (int k = 1; k <= 5; k++) {
+        //Inner for loop (1 time)
+        for (int m = 1; m <= 1; m++) {
+            //prints *
+            cout << "*";
+        }
+        //Inner for loop (53 times)
+        for (int n = 1; n <= 53; n++) {
+            //prints 53 spaces
+            cout << " ";
+        }
+        //Inner for loop (1 time)
+        for (int o = 1; o <= 1; o++) {
+            //prints *
+            cout << "*";
+        }
+        //new line
+        cout << endl;
+    }
+//Row 16 ---------------------------------------------------------------------------------------------------------------
+    //prints the corner of the room
+    cout << "\\";
+    //Seventh for loop (1-53)
+    for (int n = 1; n <= 53; n++) {
+        //prints 53 spaces
+        cout << " ";
+    }
+    //prints the corner of the room
+    cout << "/";
+    cout << endl;
+//Row 17 ---------------------------------------------------------------------------------------------------------------
+    //prints the corner of the room
+    cout << " \\";
+    //Eighth for loop (1-51)
+    for (int n = 1; n <= 51; n++) {
+        cout << " ";
+    }
+    //prints the corner of the room
+    cout << "/";
+    cout << endl;
+//Row 18 ---------------------------------------------------------------------------------------------------------------
+    //prints "\" with proper spacing
+    cout << "  \\   ";
+    //Ninth for loop (1-5)
+    for (int a = 1; a <= 5; a++) {
+        //Prints asterisk 5 times
+        cout << "*";
+        //Inner for loop (3 times)
+        for (int b = 1; b <= 3; b++) {
+            //Prints 3 spaces between each asterisk
+            cout << " ";
+        }
+    }
+    //Prints the door
+    cout << "| |   ";
+    //Tenth for loop(1-5)
+    for (int d = 1; d <= 5; d++) {
+        //Prints 5 asterisks
+        cout << "*";
+        //Inner for loop (3 times)
+        for (int y = 1; y <= 3; y++) {
+            //prints 3 spaces between asterisk
+            cout << " ";
+        }
+    }
+    //prints the corner
+    cout << "/";
+    cout << endl;
+}

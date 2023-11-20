749ca581022a9b78522e7aebfc6a043afca4ff93
JackFritschel <145804599+JackFritschel@users.noreply.github.com>
2023-11-20 17:46:25 -0600

    Map final

--- Map.cpp
+++ Map.cpp
@@ -3,98 +3,159 @@
 using namespace std;
 
 int main() {
-    //Top Row
-    cout << "          ";
-    cout << "*  *  *  *  *  *  *  *  *  *" << endl;
+    //---------top Row
+    cout << "               " ;
+    cout << "*  *  *  *  *  *  *  *" << endl;
 
     //row 2
 
-    cout << "        ";
+    cout << "            ";
     cout << "*";
     cout << "      ";
     cout << "*";
-    cout << "                 ";
+    cout << "            ";
     cout << "*";
     cout << "      ";
     cout << "*" << endl;
 
     //row 3
 
-    cout << "      ";
+    cout << "         ";
     cout << "*";
-    cout << "        ";
-    cout << "*";
-    cout << "                 ";
-    cout << "*";
-    cout << "        ";
+    cout << "         ";
+    cout << "-";
+    cout << "            ";
+    cout << "-";
+    cout << "         ";
     cout << "*" << endl;
 
     //row 4
-
-    cout << "    ";
+    cout << "      ";
     cout << "*";
-    cout << "          ";
-    cout << "=";
-    cout << "                 ";
-    cout << "=";
-    cout << "          ";
+    cout << "            ";
+    cout << "-";
+    cout << "            ";
+    cout << "-";
+    cout << "            ";
     cout << "*" << endl;
 
     //row 5
 
-    cout << "  ";
+    cout << "   ";
+    cout << "*";
+    cout << "               ";
     cout << "*";
     cout << "            ";
     cout << "*";
-    cout << "                 ";
-    cout << "*";
-    cout << "            ";
+    cout << "               ";
     cout << "*" << endl;
 
-    //row 6
+    //row 6 ----- top of center room; end of diagonal sides.
 
-    cout << "";
-    cout << "*";
-    cout << "              ";
-    cout << "*";
-    cout << "                 ";
-    cout << "*";
-    cout << "              ";
-    cout << "*" << endl;
+    cout << "*              ";
+    cout << "*  *  *  |  |  *  *  *";
+    cout << "              *" << endl;
 
     //row 7
 
-    cout << "*         ";
-    cout << "*  *  *  *  *  *  *  *  *  *";
-    cout << "         *" << endl;
-
-    //row 8
-
-    cout << "*         ";
-    cout << "*                           *";
-    cout << "         *" << endl;
+    cout << "*              ";
+    cout << "*                    *";
+    cout << "              *" << endl;
 
     //row 9
 
-    cout << "*         ";
-    cout << "*                           *";
-    cout << "         *" << endl;
+    cout << "*  *  |  |  *  ";
+    cout << "*                    *";
+    cout << "  *  |  |  *  *" << endl;
 
     //row 10
 
-    cout << "*         ";
-    cout << "*                           *";
-    cout << "         *" << endl;
+    cout << "*              ";
+    cout << "*                    *";
+    cout << "              *" << endl;
 
     //row 11
 
-    cout << "*         ";
-    cout << "*                           *";
-    cout << "         *" << endl;
+    cout << "*              ";
+    cout << "-                    -";
+    cout << "              *" << endl;
 
     //row 12
 
-    cout << "*         ";
-    cout << "*                           *";
-    cout << "         *" << endl;
+    cout << "*              ";
+    cout << "-                    -";
+    cout << "              *" << endl;
+
+    //row 13
+
+    cout << "*              ";
+    cout << "*                    *";
+    cout << "              *" << endl;
+
+    //row 14
+
+    cout << "*  *  |  |  *  ";
+    cout << "*                    *";
+    cout << "  *  |  |  *  *" << endl;
+
+    //row 15
+
+    cout << "*              ";
+    cout << "*                    *";
+    cout << "              *" << endl;
+
+    //row 16 ------ bottom row of center room
+
+    cout << "*              ";
+    cout << "*  *  *  |  |  *  *  *";
+    cout << "              *" << endl;
+
+    //row 17
+
+    cout << "   ";
+    cout << "*";
+    cout << "               ";
+    cout << "*";
+    cout << "            ";
+    cout << "*";
+    cout << "               ";
+    cout << "*" << endl;
+
+    //row 18
+
+    cout << "      ";
+    cout << "*";
+    cout << "            ";
+    cout << "-";
+    cout << "            ";
+    cout << "-";
+    cout << "            ";
+    cout << "*" << endl;
+
+    //row 19
+
+    cout << "         ";
+    cout << "*";
+    cout << "         ";
+    cout << "-";
+    cout << "            ";
+    cout << "-";
+    cout << "         ";
+    cout << "*" << endl;
+
+    //row 20
+
+    cout << "            ";
+    cout << "*";
+    cout << "      ";
+    cout << "*";
+    cout << "            ";
+    cout << "*";
+    cout << "      ";
+    cout << "*" << endl;
+
+    //------------last row
+
+    cout << "               " ;
+    cout << "*  *  *  *  *  *  *  *" << endl;
 }

1daed4376e4c5386e06e1d28ebcae144ccef80a7
ThePenguin134 <thepenguin134@gmail.com>
2023-11-29 13:24:25 -0600

    Working Build

--- BossFight.cpp
+++ BossFight.cpp
@@ -1,1 +1,43 @@
-// Cody's Working on this rn, Ill update this if I take a break.
+// Cody's Working on this rn, I'll update this if I take a break.
+
+#include "iostream"
+#include "cmath"
+
+using namespace std;
+
+void BossFight() {
+    // Snail Struct
+        // Health
+        // # of heads
+        // Attack damage
+
+    // Scalpel Function
+        // Deal 5 damage
+
+    // Shovel Function
+        //Choice to attack or wait
+
+    // Dagger Function
+        // Bool to track if Dagger was already used
+
+    // Needle Function
+        // Small chance to instakill, otherwise do nothing
+
+    // Sword Function
+        //Bool to track if sword was already used
+
+
+    // Select Choice code
+        // Let the user select any weapon or to run away.
+
+    //Actual Fight Code
+        //Repeat till snail or player dies
+            // Let User select choice
+            // Snail attacks
+
+
+
+
+
+
+}

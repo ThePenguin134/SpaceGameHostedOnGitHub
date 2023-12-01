b76466fe0f38b10521deb2534b82bb66f4376edc
ThePenguin134 <thepenguin134@gmail.com>
2023-12-01 11:35:44 -0600

    Working Build

--- BossFight.cpp
+++ BossFight.cpp
@@ -5,31 +5,72 @@
 
 using namespace std;
 
+//Snail struct with its snailHealth, # of heads, and damage before growing additional heads
+struct Snail {
+    int Health;
+    int Heads;
+    int BaseDamage;
+
+};
+// Weapon availability/damage values struct
+class Weapon {
+public:
+    bool isAvailable;
+    int damage;
+
+    Weapon(bool WeaponIsAvailable, int weaponDamage) : isAvailable(WeaponIsAvailable),
+                                                       damage(weaponDamage) {};
+};
+
+Weapon Scalpel(false, 1);
+Weapon Shovel(false, 1);
+Weapon Dagger(false, 1);
+Weapon Needle(false, 1);
+Weapon Sword(false, 1);
+Weapon RunAway(false, 1);
+
+void bossSlection() {
+    cout << "What would you like to do?" << endl;
+    cout << "1) Attack with Scalpel" << endl;
+    cout << "2) Attack with Shovel" << endl;
+    cout << "3) Attack with Dagger" << endl;
+    cout << "4) Attack with Needle" << endl;
+    cout << "5) Attack with Sword" << endl;
+    cout << "6) Run Away" << endl;
+    int bossChoice;
+    cin >> bossChoice;
+    switch (bossChoice) {
+        case 1: {
+            ScalpelUsed();
+        }
+        case 2: {
+            ShovelUsed();
+        }
+        case 3: {
+            DaggerUsed();
+        }
+        case 4: {
+            NeedleUsed();
+        }
+        case 5: {
+            SwordUsed();
+        }
+        case 6: {
+            RunAway();
+        }
+        default: {
+            cout << "Not a valid selection!" << endl;
+        };
+    }
+}
+
+void ScalpelUsed() {
+
+}
+// ___________________________________________________________________________________________________________________
+
 void BossFight() {
 
-    //Snail struct with its health, # of heads, and damage before growing additional heads
-    struct Snail {
-        int health;
-        int heads;
-        int snailBaseDamage;
-    };
-
-    // Weapon availability struct
-    class Weapon {
-    public:
-        bool isAvailable;
-        int damage;
-
-        Weapon(bool WeaponIsAvailable, int weaponDamage) : isAvailable(WeaponIsAvailable),
-                                                                              damage(weaponDamage) {};
-    };
-
-    Weapon Scalpel(false, 1);
-    Weapon Shovel(false, 1);
-    Weapon Dagger(false, 1);
-    Weapon Needle(false, 1);
-    Weapon Sword(false, 1);
-    Weapon RunAway(false, 1);
 
     // Scalpel Function
         // Deal a small amount of damage
@@ -52,46 +93,15 @@ void BossFight() {
         // Seal off the door
 
     // Select Choice code - Let the user select any weapon or to run away.
-    cout << "What would you like to do?" << endl;
-    cout << "1) Attack with Scalpel" << endl;
-    cout << "2) Attack with Shovel" << endl;
-    cout << "3) Attack with Dagger" << endl;
-    cout << "4) Attack with Needle" << endl;
-    cout << "5) Attack with Sword" << endl;
-    cout << "6) Run Away" << endl;
-    int bossChoice;
-    cin >> bossChoice;
-    switch(bossChoice) {
-        case 1: {
 
-        }
-        case 2: {
-
-        }
-        case 3: {
-
-        }
-        case 4: {
-
-        }
-        case 5: {
-
-        }
-        case 6: {
-
-        }
-        default: {
-            cout << "Not a valid selection!" << endl;
-        };
-    }
 
     //Actual Fight Code
         //Repeat till snail or player dies
-        for((Snail().health != 0) && (playerHealth != 0)) {
+        for((Snail().snailHealth != 0) && (playerHealth != 0)) {
 
-        }
-            // Let User select choice
+            bossSlection();// Let User select choice
             // Snail attacks
+        }
 
 
 

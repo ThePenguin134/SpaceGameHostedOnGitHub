4316adcbcf05cd68357ae3e9ee099cbfe9fc2ea4
ThePenguin134 <thepenguin134@gmail.com>
2023-12-04 20:35:17 -0600

    Working Build

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "cmath"
 #include "sstream"
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -1247,12 +1249,7 @@ int startInput;
 string gameInput;
 void playMenu();
 void checkInventory();
-struct Item {
-    string name;
-    string description;
-};
-
-class ItemConstructor {
+class Item {
 private:
     string name;
     string description;
